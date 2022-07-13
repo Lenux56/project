46c29698579cb1f701d466e22f925d86973f1bef
Lenux56 <popelyshelena@gmail.com>
2022-07-13 11:07:08 +0300

    5

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,27 +9,31 @@ import Logo from '../../UI/Logo'
 export default function Footer() {
   return (
     <div className={s.footer}>
-      <div className={s.label}>
+      <div className={`${s.label} ${s.item}`}>
           <Logo />
           <SocialContainer />
       </div>
+	  <div className={s.item} >
       <FooterUrls linkUrls={linksExplore}>Explore</FooterUrls>
-      <FooterUrls linkUrls={linksPages}>Utility Pages</FooterUrls>
-      <div className={`${s.menu} ${s.contacts}`}>
-					<h4 className={s.title}>Keep in Touch</h4>
-					<div className={s.innerContact}>
-						<p>Address :</p>
-						<a href="#" className={s.addres}>24A Kingston St, Los Vegas<br />NC 28202, USA.</a>
-					</div>
-					<div className={s.innerContact}>
-						<p>Mail :</p>
-						<a href="mailto:support@pages.com" className={s.mail}>support@pages.com</a>
-					</div>
-					<div className={s.innerContact}>
-						<p>Phone :</p>
-						<a href="tel:(+22) 123 - 4567 - 900" className={s.phone}>(+22) 123 - 4567 - 900</a>
-					</div>
-				</div>
-        </div>
+      </div>
+	  <div className={s.item} >
+	  <FooterUrls linkUrls={linksPages}>Utility Pages</FooterUrls>
+      </div>
+	  <div className={s.item}>
+			<h4>Keep in Touch</h4>
+			<div className={s.innerContact}>
+				<p>Address :</p>
+				<a href="#" className={s.addres}>24A Kingston St, Los Vegas<br />NC 28202, USA.</a>
+			</div>
+			<div className={s.innerContact}>
+				<p>Mail :</p>
+				<a href="mailto:support@pages.com" className={s.mail}>support@pages.com</a>
+			</div>
+			<div className={s.innerContact}>
+				<p>Phone :</p>
+				<a href="tel:(+22) 123 - 4567 - 900" className={s.phone}>(+22) 123 - 4567 - 900</a>
+			</div>
+		</div>
+    </div>
   )
 }
